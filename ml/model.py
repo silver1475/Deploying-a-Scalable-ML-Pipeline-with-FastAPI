fef5ebdab33cb2a8877b84eddd4bc2ed259fee2c
Anthony <viet1475@gmail.com>
2026-05-06 17:15:31 +0000

    updated py

--- ml/model.py
+++ ml/model.py
@@ -1,6 +1,7 @@
 import joblib
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import precision_score, recall_score, fbeta_score
+from ml.data import process_data
 
 def train_model(X_train, y_train):
     model = RandomForestClassifier(random_state=42)
@@ -22,3 +23,20 @@ def save_model(model, path):
 def load_model(path):
     return joblib.load(path)
 
+def performance_on_categorical_slice(
+    data, column_name, slice_value, categorical_features, label, encoder, lb, model
+):
+    data_slice = data[data[column_name] == slice_value]
+
+    X_slice, y_slice, _, _ = process_data(
+        data_slice,
+        categorical_features=categorical_features,
+        label=label,
+        training=False,
+        encoder=encoder,
+        lb=lb,
+    )
+
+    preds = inference(model, X_slice)
+    precision, recall, fbeta = compute_model_metrics(y_slice, preds)
+    return precision, recall, fbeta
